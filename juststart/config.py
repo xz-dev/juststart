6a5584f43aaa24020e51c9ad25c4289e83c5beab
xz-dev <xiangzhedev@gmail.com>
2023-06-04 15:47:53 +0800

    Add ENV setting

--- juststart/config.py
+++ juststart/config.py
@@ -1,1 +1,8 @@
-enable_compatible_runit = ["down"]
+import os
+
+_lowercase_env_vars = {k.lower(): v for k, v in os.environ.items()}
+
+# runit: down
+enable_compatible_runit = (
+    _lowercase_env_vars.get("compatible_runit", "").lower().split(",")
+)
